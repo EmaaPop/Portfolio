747518c6f1ed1eb2616338f17234a8593c471d92
Ema <69342876+EmaaPop@users.noreply.github.com>
2023-10-25 23:30:54 +0100

    Contacts updated

--- js.js
+++ js.js
@@ -1,5 +1,47 @@
 $(document).foundation();
 
+  //add .on-section to section currently on scrolltop, remove .onsection from neighboring sections
+  if (scrollTop > aboutPoint && scrollTop < (portfolioPoint - navHeight)) {
+    $('#about-btn').addClass('on-section');
+    $('#portfolio-btn').removeClass('on-section');
+  } else if (scrollTop > (portfolioPoint - navHeight) && scrollTop < (contactPoint - navHeight)) {
+    $('#about-btn').removeClass('on-section');
+    $('#portfolio-btn').addClass('on-section');
+    $('#contact-btn').removeClass('on-section');
+  } else if (scrollTop > (contactPoint - navHeight)) {
+    $('#portfolio-btn').removeClass('on-section');
+    $('#contact-btn').addClass('on-section');
+  }
+
+
+$('document').ready(function() {
+  var lineBorder = parseInt($('.section-seperator').css('border-top-width'), 10);
+
+  //nav buttons animate html and body(for cross-browser comp.) to anchors, anchors modified for navbar and <hr> that seperates sections
+  $('.to-home').click(function() {
+    $('html, body').animate({
+      scrollTop: $('#Home').offset().top - navHeight
+    }, 600);
+  });
+  $('.to-about').click(function() {
+    $('html,body').animate({
+      scrollTop: $('#about-anchor').offset().top - navHeight - aboutPadding + lineBorder
+    }, 600);
+  });
+  $('.to-portfolio').click(function() {
+    $('html, body').animate({
+      scrollTop: $('#portfolio-anchor').offset().top - navHeight + lineBorder
+    }, 600);
+  });
+  $('.to-contact').click(function() {
+    $('html, body').animate({
+      scrollTop: $('#contact-anchor').offset().top - navHeight + lineBorder
+    }, 600);
+  });
+
+});
+
+
 
 var fov = 2000;
 
